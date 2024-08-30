dac85bd2cd6e64857a231192fbb539d39d54c920
WellingtonLFaria <wellingtonll.faria@gmail.com>
2024-08-30 11:42:24 -0300

    Initial Setup

---
+++ app/(tabs)/index.tsx
@@ -0,0 +1,9 @@
+import { View, Text } from "react-native";
+
+export default function Index() {
+	return (
+		<View className="h-screen p-2">
+			<Text className="text-center text-2xl">Index</Text>
+		</View>
+	);
+}
